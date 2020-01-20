267a4b5c5c4d534d5250de02b4ab4a86ea8bf127
Techno-Disaster <nitinnirve@gmail.com>
2020-01-21 03:33:48 +0530

    cleaned a lot, time to add database

--- main.py
+++ main.py
@@ -1,55 +1,18 @@
 import requests
-import json
-from datetime import datetime
-from bs4 import BeautifulSoup
+import time
+import html
+prev_last_time = time.time()
 
+appearing_order = ["type", "votes", "com", "title"]
+print("Time            Action  Votes Com   Title")
 
-def firehose():
-    previousts = 0
-    duplicateeventts = 0
-    ts_duplicate_counter = 1
-    while True:
-        r = requests.get(
-            "https://www.meneame.net/backend/sneaker2").content
-        data = json.loads(r)  # we get data here.
-
-        listofdata = data["events"]
-        reverselist = listofdata[::-1]  # to reverse the list
-
-        for event in reverselist:
-
-            if int(event["ts"]) < int(previousts):
-                continue
-
-            if int(event["ts"]) == int(previousts):  # duplicate event.
-                ts_duplicate_counter += 1
-                if ts_duplicate_counter <= duplicateeventts:
-                    continue
-            output(event)
-            previousts = event["ts"]  # this keeps updating the previous ts.
-
-        duplicateeventts = ts_duplicate_counter
-        ts_duplicate_counter = 0  # reset ts counter for next loop
-
-
-def output(event):
-    time = str(datetime.utcfromtimestamp(
-        int(event["ts"])).strftime('%Y-%m-%d %H:%M:%S'))
-    sub_name = event["sub_name"]
-    action = event["type"]
-    vote_comments = str(event["votes"]) + "/" + str(event["com"])
-    title = event["title"]
-    ret = str(BeautifulSoup("{} | {:<10} | {:<10} | {:<8} | {}"
-                            .format(time, sub_name, action, vote_comments, title,), 'html.parser'))
-
-    print(ret)
-
-    # for row in data['event']['data']:
-
-    #     data = (time, sub_name, vote_comments, title, action)
-    #     dataframe.append(data)
-
-
-if __name__ == "__main__":
-    print("        Time        |    Topic   |   Action   | Comments |   Title   ")
-    firehose()
+while True:
+    response = requests.get(
+        "https://www.meneame.net/backend/sneaker2?time=" + str(prev_last_time))
+    events = response.json()["events"]
+    for event in events:
+        print(time.strftime("%H:%M:%S", time.localtime(
+            int(event["ts"]))), end="\t")
+        print("\t".join(html.unescape(
+            str(event[item])) for item in appearing_order))
+        prev_last_time = max(int(event["ts"]), prev_last_time)
